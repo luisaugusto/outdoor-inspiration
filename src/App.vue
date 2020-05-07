00e81b671af55aa9705565abbbfade1ec14508ba
Luis Augusto <hello@luis.codes>
2020-05-07 09:47:46 -0700

    fixes

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
     <span v-for="park in parks" :key="park.slug">{{ park.title }}</span>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  },
   computed: {
     parks() {
       return this.$store.state.parks;
@@ -24,14 +16,3 @@ export default {
   }
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
